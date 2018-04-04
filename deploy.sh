004fb5b6f82378350bd209dd5cc6d070db94995f
Stuart Craigen <s.craigen@mac.com>
2018-04-04 15:10:32 +0100

    Migrate to ACP

--- deploy.sh
+++ deploy.sh
@@ -1,12 +1,33 @@
-#!/usr/bin/env bash
+#!/bin/bash
 
-if [ $ENVIRONMENT == "prod" ]
-then
-    export KUBE_TOKEN=${PROD_KUBE_TOKEN}
+export KUBE_NAMESPACE=${KUBE_NAMESPACE}
+export KUBE_SERVER=${KUBE_SERVER}
+
+if [[ -z ${VERSION} ]] ; then
+    export VERSION=${IMAGE_VERSION}
 fi
 
-env
+if [[ ${ENVIRONMENT} == "pr" ]] ; then
+    echo "deploy ${VERSION} to pr namespace, using PTTG_IP_PR drone secret"
+    export KUBE_TOKEN=${PTTG_IP_PR}
+else
+    if [[ ${ENVIRONMENT} == "test" ]] ; then
+        echo "deploy ${VERSION} to test namespace, using PTTG_IP_TEST drone secret"
+        export KUBE_TOKEN=${PTTG_IP_TEST}
+    else
+        echo "deploy ${VERSION} to dev namespace, using PTTG_IP_DEV drone secret"
+        export KUBE_TOKEN=${PTTG_IP_DEV}
+    fi
+fi
+
+if [[ -z ${KUBE_TOKEN} ]] ; then
+    echo "Failed to find a value for KUBE_TOKEN - exiting"
+    exit -1
+fi
+
 cd kd
-kd --insecure-skip-tls-verify --timeout 5m0s \
-   --file service.yaml \
-   --file deployment.yaml
+
+kd --insecure-skip-tls-verify \
+    -f networkPolicy.yaml \
+    -f deployment.yaml \
+    -f service.yaml
